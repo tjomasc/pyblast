4e66aedc28d87217bcc00cb447d158457b917baa
Thomas Craig <thomas.craig@tjc.me.uk>
2014-07-15 13:30:05 +0100

    Updated setup files

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
     name = 'pyblast',
-    version = '0.1.1',
+    version = '0.1.2',
     packages = ['pyblast'],
     include_package_data = True,
     zip_safe = False,
